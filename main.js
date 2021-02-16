207458b3e0bdc4acf1aed4e46a51801824c4bdda
Hiroki_S <sebasuta@gmail.com>
2021-02-16 20:21:22 +0900

    fifth

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+$("[data-trigger]").on("click", function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    var offcanvas_id =  $(this).attr('data-trigger');
+    $(offcanvas_id).toggleClass("show");
+    $('body').toggleClass("offcanvas-active");
+    $(".screen-overlay").toggleClass("show");
+}); 
+
+$(".btn-close, .screen-overlay").click(function(e){
+    $(".screen-overlay").removeClass("show");
+    $(".mobile-offcanvas").removeClass("show");
+    $("body").removeClass("offcanvas-active");
+}); 
+
 const backToTop =()=>{
     if($(window).scrollTop() >= 500){
         $('.back-to-top').fadeIn();
@@ -18,3 +33,4 @@ $('.back-to-top').on('click', (e)=>{
 });
 
 backToTop();
+
